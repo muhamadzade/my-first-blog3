f9f88f52c2e6410c667e702d79c4da25b425e4e6
muhamadzade <muhamadzade@gmail.com>
2021-09-28 14:50:15 +0330

    ph5

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.post_list,name='post_list'),
     path('hello/', views.hello,name='hello'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new,name='post_new'),
+
 ]
