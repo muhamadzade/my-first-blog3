1e751d0d619d89e9a4b3b1a514e7a73a82fd0598
muhamadzade <muhamadzade@gmail.com>
2021-09-28 13:05:41 +0330

    ph4

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.post_list,name='post_list'),
+    path('hello/', views.hello,name='hello'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
